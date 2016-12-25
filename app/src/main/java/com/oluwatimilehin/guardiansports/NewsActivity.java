5cff31e4e51a333289a68aa437100f058035a429
Oluwatimilehin <timad97@Yahoo.com>
2016-12-25 16:23:29 +0100

    Set list adapter layout

--- app/src/main/java/com/oluwatimilehin/guardiansports/NewsActivity.java
+++ app/src/main/java/com/oluwatimilehin/guardiansports/NewsActivity.java
@@ -2,12 +2,22 @@ package com.oluwatimilehin.guardiansports;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NewsActivity extends AppCompatActivity {
+
+    NewsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.list);
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+        list.setAdapter(mAdapter);
     }
+
 }
