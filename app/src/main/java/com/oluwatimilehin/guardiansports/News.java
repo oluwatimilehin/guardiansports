e6d8b0a99c014ef225a94152017aeffc2547877e
Oluwatimilehin <timad97@Yahoo.com>
2016-12-25 15:40:28 +0100

    Create News class and add getter methods

--- app/src/main/java/com/oluwatimilehin/guardiansports/News.java
+++ app/src/main/java/com/oluwatimilehin/guardiansports/News.java
@@ -5,4 +5,25 @@ package com.oluwatimilehin.guardiansports;
  */
 
 public class News {
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    protected String mTitle, mDate, mUrl;
+
+
+    public News(String title, String date, String url){
+        mDate = date;
+        mTitle = title;
+        mUrl = url;
+    }
 }
---
+++ app/src/main/java/com/oluwatimilehin/guardiansports/NewsAdapter.java
@@ -0,0 +1,8 @@
+package com.oluwatimilehin.guardiansports;
+
+/**
+ * Created by timad on 25/12/2016.
+ */
+
+public class NewsAdapter {
+}
