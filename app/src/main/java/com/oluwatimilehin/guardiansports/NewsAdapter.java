3cd8642b0eb0f9bfc3f9ecd08e7f2cbf9a9fa2f3
Oluwatimilehin <timad97@Yahoo.com>
2016-12-25 16:13:25 +0100

    Create adapter and set layout for a list item

--- app/src/main/java/com/oluwatimilehin/guardiansports/NewsAdapter.java
+++ app/src/main/java/com/oluwatimilehin/guardiansports/NewsAdapter.java
@@ -1,8 +1,39 @@
 package com.oluwatimilehin.guardiansports;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by timad on 25/12/2016.
  */
 
-public class NewsAdapter {
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter(NewsActivity context, ArrayList<News> news){
+        super(context, 0, news);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false
+            );
+        }
+        News currentNews = getItem(position);
+
+        TextView headlineTextView = (TextView) listItemView.findViewById(R.id.headline);
+        headlineTextView.setText(currentNews.getTitle());
+
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+        dateTextView.setText(currentNews.getDate());
+
+        return  listItemView;
+    }
 }
